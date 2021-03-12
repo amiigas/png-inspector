393eeadc36574f0975f5583e1c9a43d5e657e287
Aleksy Migasiewicz <aleksy.migasiewicz@nokia.com>
2021-03-12 01:03:47 +0100

    Add iCCP chunk printer

--- inspector.py
+++ inspector.py
@@ -1,5 +1,9 @@
 from PIL import Image as PILImage
+from PIL import ImageCms as PILImageCms
+import zlib
+import io
 import lookup_tables as lt
+
 
 class Image:
   def __init__(self):
@@ -24,7 +28,6 @@ class Image:
     print("Correct png file signature.")
     return True
 
-
   def index_chunks(self):
     print("Indexing all chunks.")
     start = 8
@@ -34,14 +37,12 @@ class Image:
       start = start + 8 + chunk.datasize + 4
     print("Found", len(self.chunks), "chunks.\n")
 
-
   def get_chunk_at(self, start):
     datasize = int.from_bytes(self.data[start:start+4], byteorder="big", signed=False)
     name = ""
     for offset in range(4,8):
       name += chr(self.data[start + offset])
     return Chunk(start, name, datasize)
-
 
   def print_chunk_named(self, name):
     if name == "IHDR":
@@ -52,14 +53,14 @@ class Image:
       self.print_IDAT_chunks()
     elif name == "IEND":
       self.print_IEND_chunk()
-
+    elif name == "iCCP":
+      self.print_iCCP_chunk()
 
   def print_critical_chunks(self):
     self.print_IHDR_chunk()
     self.print_PLTE_chunk()
     self.print_IDAT_chunks()
     self.print_IEND_chunk()
-
 
   def print_IHDR_chunk(self):
       for chunk in self.chunks:
@@ -69,22 +70,21 @@ class Image:
           height = int.from_bytes(chunk_data[4:8], byteorder="big", signed=False)
           bit_depth = chunk_data[8]
           color_type = chunk_data[9]
-          text = '{0:15}{1:<8}\n'.format("chunk name:", chunk.name)
-          text += '{0:15}{1:<8}\n'.format("width [px]:", width)
-          text += '{0:15}{1:<8}\n'.format("height [px]:", height)
-          text += '{0:15}{1:<8}\n'.format("bit_depth:", bit_depth)
-          text += '{0:15}{1:<8}{2}\n'.format("color_type:", color_type, lt.ihdr_color_type[color_type])
+          text = '{0:16}{1:<}\n'.format("chunk name:", chunk.name)
+          text += '{0:16}{1:<}\n'.format("width [px]:", width)
+          text += '{0:16}{1:<}\n'.format("height [px]:", height)
+          text += '{0:16}{1:<}\n'.format("bit_depth:", bit_depth)
+          text += '{0:16}{1:<}{2}\n'.format("color_type:", color_type, lt.ihdr_color_type[color_type])
           print(text)
-
 
   def print_PLTE_chunk(self):
     for chunk in self.chunks:
       if chunk.name == "PLTE":
         chunk_data = self.data[chunk.start+8:chunk.start+8+chunk.datasize]
         palette_size = chunk.datasize//3
-        text = '{0:15}{1}\n'.format("chunk name:", chunk.name)
+        text = '{0:16}{1}\n'.format("chunk name:", chunk.name)
         text += '{0:8}{1:>3} {2:>3} {3:>3}\n'.format("color:", "R", "G", "B")
-        text += '{0}\n'.format("-------------------")
+        text += '{0}\n'.format("--------------------")
         print(text)
         for color in range(palette_size):
           red = chunk_data[color*3]
@@ -92,18 +92,42 @@ class Image:
           blue = chunk_data[color*3+2]
           print('{0:<8}{1:3} {2:3} {3:3}\n'.format(color, red, green, blue))
 
-
   def print_IDAT_chunks(self):
     for chunk in self.chunks:
       if chunk.name == "IDAT":
         print(chunk)
-
 
   def print_IEND_chunk(self):
     for chunk in self.chunks:
       if chunk.name == "IEND":
         print(chunk)
 
+  def print_iCCP_chunk(self):
+    for chunk in self.chunks:
+      if chunk.name == "iCCP":
+        chunk_data = self.data[chunk.start+8:chunk.start+8+chunk.datasize]
+        profile_name = []
+        for i in chunk_data:
+          if not i == 0x00:
+            profile_name.append(i)
+          else:
+            break
+        profile_name = "".join(list(map(chr, profile_name)))
+        compression_type = chunk_data[len(profile_name)+1]
+        compressed_icc_profile = chunk_data[len(profile_name)+2:]
+        icc_profile = zlib.decompress(compressed_icc_profile)
+        f_stream = io.BytesIO(icc_profile)
+        profile = PILImageCms.ImageCmsProfile(f_stream)
+        text = '{0:16}{1:<}\n'.format("chunk name:", chunk.name)
+        text += '{0:16}{1:<}\n'.format("profile name:", profile_name)
+        text += '{0:16}{1:<}\n'.format("compression:", compression_type)
+        text += '{0:16}{1:}\n'.format("creation date:", profile.profile.creation_date)
+        text += '{0:16}{1:<}\n'.format("version:", profile.profile.version)
+        text += '{0:16}{1:<}\n'.format("color space:", profile.profile.xcolor_space)
+        text += '{0:16}{1:<}\n'.format("device class:", profile.profile.device_class)
+        text += '{0:16}{1:<}\n'.format("copyright:", profile.profile.copyright)
+        text += '{0:16}{1:<}\n'.format("description:", profile.profile.profile_description)
+        print(text)
 
   def print_all_chunks(self):
     for chunk in self.chunks:
@@ -116,20 +140,18 @@ class Chunk:
     self.name = name
     self.datasize = datasize
 
-
   def __str__(self):
-    text = '{0:18}{1}\n'.format("chunk name:", self.name)
-    text += '{0:18}{1}\n'.format("starting index:", self.start)
-    text += '{0:18}{1}\n'.format("size of data:", self.datasize)
+    text = '{0:16}{1}\n'.format("chunk name:", self.name)
+    text += '{0:16}{1}\n'.format("starting index:", self.start)
+    text += '{0:16}{1}\n'.format("size of data:", self.datasize)
     return text
 
 
 def display_greetings():
   print("\nWelcome to the PNG Inspector!")
 
-
 def menu():
-  text = "======= MENU =======\n"
+  text = "========== MENU ==========\n"
   text += "1 - Display image\n"
   text += "2 - Print chunk list\n"
   text += "3 - Print critical chunks\n"
@@ -139,7 +161,6 @@ def menu():
   text += "q - Exit\n"
   text += "Your choice: "
   return text
-
 
 def pretty_print(bytes, linewidth):
   i = 1
@@ -151,11 +172,9 @@ def pretty_print(bytes, linewidth):
     i += 1
   print("")
 
-
 def display_image(filepath):
   img = PILImage.open(filepath)
   img.show()
-
 
 def query_filename():
   filepath = input("Enter png image file path. ('q' to quit): ")
@@ -163,7 +182,6 @@ def query_filename():
     exit()
   else:
     inspect(filepath)
-
 
 def inspect(filepath):
   img = Image()
@@ -180,16 +198,16 @@ def inspect(filepath):
           img.print_critical_chunks()
         elif option == "5":
           chunk_name = input("Enter name of chunk to print: ")
+          print("")
           img.print_chunk_named(chunk_name)
         option = input(menu())
         print("")
-
 
 def main():
   display_greetings()
   while True:
     query_filename()
-  
+
 
 if __name__== "__main__":
   main()

41339c9514eff21bd063b21d6997a016cf2d34d8
Aleksy Migasiewicz <aleksy.migasiewicz@gmail.com>
2021-05-08 17:25:35 +0200

    Enable RSA encoding/decoding

--- rsa.py
+++ rsa.py
@@ -1,5 +1,4 @@
 import secrets
-import math
 
 
 def is_prime(n, iterations=100):
@@ -21,14 +20,44 @@ def random_prime(bits):
         n = random_number(bits)
     return n
 
-def RSA_key(bits):
+def euler(p, q):
+    return (p-1) * (q-1)
+
+def mod_mul_inv(a, m):
+    return pow(a, -1, m)
+
+def generate_keys(bits, e=65537):
     p = random_prime(bits//2)
     q = random_prime(bits//2)
     n = p * q
-    return n
+    phi = euler(p, q)
+    d = mod_mul_inv(e, phi)
+    return (e, n), (d, n)
 
+def encode(message, public_key):
+    e = public_key[0]
+    n = public_key[1]
+    c = pow(message, e, n)
+    return c
+
+def decode(ciphertext, private_key):
+    d = private_key[0]
+    n = private_key[1]
+    m = pow(ciphertext, d, n)
+    return m
 
 if __name__ == '__main__':
     bits = 1024
-    n = RSA_key(bits)
-    print(f"{bits}-bit RSA key: {n}")
+    public, private = generate_keys(bits)
+
+    plaintext = "poggers"
+    padded = [ord(val) for val in plaintext]
+    ciphertext = [encode(val, public) for val in padded]
+    decoded = [decode(val, private) for val in ciphertext]
+    decoded_plaintext = [chr(val) for val in decoded]
+
+    print(f"plaintext: {plaintext}")
+    print(f"padded: {padded}")
+    print(f"ciphertext: {ciphertext}")
+    print(f"decoded: {decoded}")
+    print(f"decoded_plaintext: {decoded_plaintext}")
